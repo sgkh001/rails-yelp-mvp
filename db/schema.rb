bf56dd075d93ad504ac5f65f9961aa9dcef5f364
SHAHEEN GOKHOOL <bshine772@gmail.com>
2023-01-11 11:38:14 +0400

    add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_072541) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_073549) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
     t.string "phone_number"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
